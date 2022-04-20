374da9f34ce01cac513b6d2f051b845451d1b7ff
Agnieszka Szajnoga <agnieszka.szajnoga@ML-1280.local>
2022-04-20 17:05:41 +0100

    Add csv file and parser

--- csv_parser.rb
+++ csv_parser.rb
@@ -7,3 +7,11 @@ titles.each do |title|
   puts titles[0]
 end
 
+
+
+
+# Write a new acceptance test that asserts: when the user visits the homepage, the page content should include each show title in the provided CSV file.
+# Configure your Rails app to use Haml for the views.
+# Create a new controller to show all shows. Make sure you're following the Rails naming conventions!
+# Create a new route so that users visiting the root of your application are directed to the index action of your new controller. Make sure you're following the Rails routing conventions!
+# Pass the acceptance test by displaying all shows from the provided CSV file file.
